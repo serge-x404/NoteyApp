4cfb4b2bafea862b8f7f14c59b45846e6e5cb276
serge-x404 <kpdev1124@gmail.com>
2026-02-23 17:47:34 +0530

    Repository created

---
+++ app/src/main/java/com/serge/noteyapp/repository/NotesRepository.kt
@@ -0,0 +1,13 @@
+package com.serge.noteyapp.repository
+
+import androidx.lifecycle.LiveData
+import com.serge.noteyapp.roomdb.Note
+import com.serge.noteyapp.roomdb.NoteDao
+
+class NotesRepository(private val noteDao: NoteDao) {
+    val allNotes: LiveData<List<Note>> = noteDao.getAllNotes()
+
+    suspend fun insertNote(note: Note){
+        return noteDao.insert(note)
+    }
+}
