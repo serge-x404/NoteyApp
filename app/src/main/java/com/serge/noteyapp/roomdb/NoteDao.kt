c95604b0ce318d276632ed6a35152047d9a52900
serge-x404 <kpdev1124@gmail.com>
2026-02-23 17:40:11 +0530

    DB Instance created

---
+++ app/src/main/java/com/serge/noteyapp/roomdb/NoteDao.kt
@@ -0,0 +1,17 @@
+package com.serge.noteyapp.roomdb
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface NoteDao {
+    // Define methods for various DB Ops.
+
+    @Insert
+    suspend fun insert(note: Note)
+
+    @Query("SELECT * FROM notes_table")
+    fun getAllNotes(): LiveData<List<Note>>
+}
---
+++ app/src/main/java/com/serge/noteyapp/roomdb/NotesDB.kt
@@ -0,0 +1,32 @@
+package com.serge.noteyapp.roomdb
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], version = 1)
+abstract class NotesDB: RoomDatabase() {
+    abstract val notesDao: NoteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: NotesDB? = null
+
+        fun getInstance(context: Context): NotesDB {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+
+                    instance = Room.databaseBuilder(
+                        context = context.applicationContext,
+                        NotesDB::class.java,
+                        "notes_db"
+                    ).build()
+                }
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
