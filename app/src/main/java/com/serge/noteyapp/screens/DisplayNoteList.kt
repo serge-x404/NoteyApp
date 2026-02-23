da9007c1c0d58d7523e787a69dbb154c4a2257ca
serge-x404 <kpdev1124@gmail.com>
2026-02-23 19:45:25 +0530

    View completed

---
+++ app/src/main/java/com/serge/noteyapp/screens/DisplayNoteList.kt
@@ -0,0 +1,24 @@
+package com.serge.noteyapp.screens
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.serge.noteyapp.roomdb.Note
+
+@Composable
+fun DisplayNoteList(notes: List<Note>){
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Fixed(2),
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        items(notes) {
+            note -> NoteListItem(note)
+        }
+    }
+}
